e17c32f8a1dd322c67b623f1b54766c08ce50ed7
Reyn-L <Reyn.Leong@gmail.com>
2017-07-22 18:53:53 -1000

    3rd test

--- indexing.sql
+++ indexing.sql
@@ -5,5 +5,8 @@
 --SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
 --57.958ms
 
-SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
+--SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
 --57.540ms
+
+--SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+--52.947ms
