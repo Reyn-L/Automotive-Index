ab35e1389c89776e87012c081f695a04dcc46b72
Reyn-L <Reyn.Leong@gmail.com>
2017-07-22 18:59:04 -1000

    all timing tests done

--- indexing.sql
+++ indexing.sql
@@ -1,15 +1,18 @@
--- CREATE USER indexed_cars_user WITH ENCRYPTED PASSWORD 'secret';
--- CREATE DATABASE indexed_cars WITH OWNER indexed_cars_user;
--- \c indexed_cars
+CREATE USER indexed_cars_user WITH ENCRYPTED PASSWORD 'secret';
+CREATE DATABASE indexed_cars WITH OWNER indexed_cars_user;
+\c indexed_cars
 
---SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
+SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
 --57.958ms
 
---SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
+SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
 --57.540ms
 
---SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
 --52.947ms
 
---SELECT * FROM car_models WHERE year >=  2010 AND year <= 2015;
+SELECT * FROM car_models WHERE year >=  2010 AND year <= 2015;
 --155.683ms
+
+SELECT * FROM car_models WHERE year = 2010;
+--67.812ms
